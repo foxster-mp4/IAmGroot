5e85ae3427780b9e04601f026175977ca45c4240
foxster-mp4 <foxster.utube@gmail.com>
2022-08-05 19:18:14 +0700

    Refactor code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $.getJSON("https://raw.githubusercontent.com/foxster-mp4/NowShowing/master/data.json", function (data) {
+    $.getJSON("data.json", function (data) {
         // Poster & background 
-        $("#poster-container").html(`<img src="${data.posters[Math.floor(Math.random() * data.posters.length)]}" id="poster" alt="poster">`)
-        setBackground()
+        $("#poster-container").html(`<img src="${data.posters[Math.floor(Math.random() * data.posters.length)]}" id="poster" alt="poster">`);
+        setBackground();
 
         // Title
         if (data.name != null) {
@@ -25,20 +25,20 @@ $(document).ready(function() {
         // Trailer
         if (data.trailerURL != null) {
             $("#trailer").attr("href", data.trailerURL);
-            $("#trailer").removeClass("disabled")
+            $("#trailer").removeClass("disabled");
+            $("#trailer-button-text").text("Watch Trailer");
         }
         
         const episodes = data.episodes;
         if (episodes == null || episodes.length == 0) {
-            $("#watch-button").addClass("disabled");
-            $("#watch-text").text("Coming Soon");
+            $("#main-button").addClass("disabled");
+            $("#main-button-text").text("Coming Soon");
             return;
         }
         
         let latestEpisode;
         if (episodes.length > 1) { // More than 1 episode; add button to select episode
-            let episodeOptions = ""
-            
+            let episodeOptions = "";
             for (let i = 0; i < episodes.length; i++) {
                 const episode = episodes[i];
                 episodeOptions += `<li><a class="dropdown-item`;
@@ -49,6 +49,8 @@ $(document).ready(function() {
                 episodeOptions += `" href="${episode.url}" n=${i}>`
                 if (episode.number != null)
                     episodeOptions += `Episode ${episode.number}`;
+                else 
+                    episodeOptions += `bruh`;
                 // if (episode.number != null && episode.name != null)
                 //     episodeOptions += " – "
                 // if (episode.name != null)
@@ -57,7 +59,7 @@ $(document).ready(function() {
             }
 
             // Add option to select episode
-            $("#watch-button-container").append(`
+            $("#main-button-container").append(`
                 <button type="button" class="btn btn-outline-light dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                     <span class="visually-hidden">Toggle Dropdown</span>
                 </button>
@@ -67,26 +69,48 @@ $(document).ready(function() {
             `);
         } else latestEpisode = episodes[0]; // 1 episode (movie)
 
-        $("#watch-button").removeClass("disabled");
+        updateMainButton(latestEpisode); // Function hoisting example
 
-        // Watch button text
-        if (latestEpisode.url == null || latestEpisode.url.trim().length == 0) { // No URL
-            $("#watch-button").addClass("disabled");
-            $("#watch-text").text("Coming Soon");
-        }  
-        else if (latestEpisode.number != null) // Has episode number
-            $("#watch-text").text(`Watch Episode ${latestEpisode.number}`);
-        // else if (latestEpisode.name != null) // Has episode name
-        //     $("#watch-text").text(`Watch ${latestEpisode.name}`);
-        else // Default
-            $("#watch-text").text("Watch Now");
-        
-        // Watch button href
-        $("#watch-button").attr("href", latestEpisode.url);
+        function updateMainButton(episode) { // Must not use arrow function since it won't be hoisted
+            // Set "play" icon
+            $("#main-button-icon").addClass("bi-play-fill").removeClass("bi-arrow-down-circle");
+            $("#main-button").removeClass("disabled"); // Enable button
+            
+            let episodeTitle = "";
+            if (episode.number != null) {
+                episodeTitle = `Episode ${episode.number}`;
+            } else if (episode.name != null) {
+                episodeTitle = episode.name;
+            } else {
+                episodeTitle = "Now";
+            }
+
+            console.log(episode.url)
+            
+            // Main URL is available
+            if (episode.url != null && episode.url.trim().length > 0) {
+                $("#main-button-text").text(`Watch ${episodeTitle}`);
+                $("#main-button").attr("href", episode.url);
+            } 
+
+            // Download URL is available
+            else if (episode.downloadURL != null && episode.downloadURL.trim().length > 0) { 
+                $("#main-button-icon").removeClass("bi-play-fill").addClass("bi-arrow-down-circle"); // Set "download" icon
+                $("#main-button-text").text(`Download ${episodeTitle}`);
+                $("#main-button").attr("href", episode.downloadURL);
+            }
+
+            // No URL available
+            else {
+                $("#main-button").addClass("disabled");
+                $("#main-button-text").text("Coming Soon");
+                $("#main-button").attr("href", "#");
+            }
+        }
 
         // Episode option click listener
         $(".dropdown-item").click(function(event) {
-            event.preventDefault(); // Prevent page reload
+            event.preventDefault(); // Prevent page reload since dropdown items are <a> elements
 
             const n = parseInt($(this).attr("n"));
             const episode = episodes[n];
@@ -94,16 +118,8 @@ $(document).ready(function() {
             $(".selected").removeClass("selected");
             $(this).addClass("selected");
 
-            if (episode.url == null || episode.url.trim().length == 0) {
-                $("#watch-text").text("Coming Soon");
-                $("#watch-button").addClass("disabled");
-            } else if (episode.url != null) {
-                $("#watch-text").text(`Watch Episode ${episode.number}`);
-                $("#watch-button").removeClass("disabled");
-            }
-            
-            $("#watch-button").attr("href", episode.url);
-        })
+            updateMainButton(episode);
+        });
     });
 });
 
